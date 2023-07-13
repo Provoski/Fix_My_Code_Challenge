f7976739a7918dec606801415ef4b4795a8d0826
Provoski <provo.iyen27@gmail.com>
2023-07-13 03:19:44 -0700

    updated react-blog

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ Index view
 """
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, Flask
 
 app_views = Blueprint('app_views', __name__)
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
 @app_views.route('/api/v1/status', methods=['GET'])
 def status():
     """ Status of the web server
     """
     return jsonify({"status": "OK"})
+
+
+if __name__ == '__main__':
+    app.run();
